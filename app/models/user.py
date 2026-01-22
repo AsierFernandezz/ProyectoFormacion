e3f5a51a58bfbe7e9173df11aee3810d3daa421c
afernandez <afernandez@cimico.tech>
2026-01-22 11:20:09 +0100

    add user models for API request and response

---
+++ app/models/user.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from pydantic import BaseModel
+
+# lo que recibe la API
+class UserCreate(BaseModel):
+    id: int
+    name: str
+    email: str
+    password: str
+    is_active: bool
+    role: str
+
+# lo que devuelve la API
+class UserPublic(BaseModel):
+    id: int
+    name: str
+    email: str
+    created_at: datetime
+    role: str
