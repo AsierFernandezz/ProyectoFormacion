0e158cb076e7270031d5c4dfeec6b436a31b510c
afernandez <afernandez@cimico.tech>
2026-01-22 14:26:27 +0100

    añadiendo repository y adapantado service user

---
+++ app/repository/user_repo.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from typing import List, Optional, Dict
+
+fake_db: List[dict] = [{"id": 1, "name": "Paco", "email": "paco@gmail.com", "created_at": datetime.now()}]
+
+def add_user(user_dict: Dict) -> Dict:
+    fake_db.append(user_dict)
+    return user_dict
+
+def get_all_users() -> List[Dict]:
+    return fake_db
+
+def get_user_by_email(email:str) -> Optional[Dict]:
+    for user in fake_db:
+        if user["email"] == email:
+            return user
+    return None
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,14 +1,14 @@
 from datetime import datetime
 from typing import List
-from app.models import UserCreate, UserPublic
 from passlib.context import CryptContext
+from app.models import UserCreate, UserPublic
+from app.repository.user_repo import fake_db, get_user_by_email, get_all_users, add_user
 
-fake_db: List[dict] = [{"id": 1, "name": "Paco", "email": "paco@gmail.com", "created_at": datetime.now()}]
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def create_user(user:UserCreate = None) -> UserPublic:
     for existing_user in fake_db:
-        if existing_user['email'] == user.email:
+        if get_user_by_email(user.email):
             raise ValueError("Este email ya existe")
 
     # hashear la contraseña
@@ -16,26 +16,26 @@ def create_user(user:UserCreate = None) -> UserPublic:
 
     # crear usuario
     new_user = {
-        "id": user.id,
+        "id": len(get_all_users()) +1,
         "name": user.name,
         "email": user.email,
         "password": hashed_password,
         "created_at": datetime.now()
     }
-    fake_db.append(new_user)
-    return UserPublic(id=user.id, name=user.name, email=user.email, created_at=new_user['created_at'])
+    added_user = add_user(new_user)
+    return UserPublic(id=added_user['id'], name=added_user['name'], email=added_user['email'], created_at=added_user['created_at'])
 
 def get_user(email: str) -> UserPublic:
-    for existing_user in fake_db:
-        if existing_user['email'] == email:
-            return UserPublic(
-                id=existing_user['id'],
-                name=existing_user['name'],
-                email=existing_user['email'],
-                created_at=existing_user['created_at']
-            )
+    existing_user = get_user_by_email(email)
+    if not existing_user:
+        raise ValueError(f"No se ha encontrado ningun usuario con el email {email}")
+    return UserPublic(
+        id=existing_user['id'],
+        name=existing_user['name'],
+        email=existing_user['email'],
+        created_at=existing_user['created_at']
+    )
     # Si no encontramos ningún usuario, lanzamos el error
-    raise ValueError(f"No se ha encontrado ningun usuario con el email {email}")
 
 def get_users() -> List[UserPublic]:
     return  [
@@ -45,5 +45,5 @@ def get_users() -> List[UserPublic]:
             email=user['email'],
             created_at=user['created_at']
         )
-        for user in fake_db
+        for user in get_all_users()
     ]
