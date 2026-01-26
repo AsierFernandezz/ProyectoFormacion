a4b8596d293c32cdfba1cb422f5ca7da6faac84f
afernandez <afernandez@cimico.tech>
2026-01-26 11:32:21 +0100

    root default message eliminado

--- app/main.py
+++ app/main.py
@@ -2,14 +2,5 @@ from fastapi import FastAPI
 from app.routers import users
 import app.routers.auth as auth
 app = FastAPI()
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
 app.include_router(users.router, tags=["users"])
 app.include_router(auth.router, tags=["auth"])
